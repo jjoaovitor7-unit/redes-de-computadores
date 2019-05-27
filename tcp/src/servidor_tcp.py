33b4f6c9db19152b12dd080183c497edfc96be4d
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-05-27 14:48:31 -0300

    atualização em servidor_tcp.py

--- tcp/src/servidor_tcp.py
+++ tcp/src/servidor_tcp.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import socket
+import datetime
 
-HOST = '127.0.0.1' # Endereco IP do Servidor
-PORT = 8080        # Porta que o Servidor esta
+HOST = '127.0.0.1' # Endereço IP do Servidor
+PORT = 8080        # Porta que o Servidor está
 
 tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 orig = (HOST, PORT)
@@ -11,14 +12,26 @@ tcp.bind(orig)
 tcp.listen()
 
 while True:
+    conexao = datetime.datetime.now()
+    conexao_horario = '[' + str(conexao.hour) + ':' + str(conexao.minute) + ':' + str(conexao.second) + ']'
     conn, cliente = tcp.accept()
+    
     cliente = str(cliente).replace('(', '[')
     cliente = str(cliente).replace(')', ']')
-    print('[Conexão]', str(cliente))
+    
+    print(conexao_horario, '[Conexão]', str(cliente))
+    
     while True:
         msg = conn.recv(1024)
+        
         if not msg:
             break
-        print(cliente, msg)   
-    print('Finalizando conexão do cliente', cliente)
+            
+        mensagem = datetime.datetime.now()
+        mensagem_horario = '[' + str(mensagem.hour) + ':' + str(mensagem.minute) + ':' + str(mensagem.second) + ']'
+        print(mensagem_horario, cliente, msg)
+    
+    desconexao = datetime.datetime.now()
+    desconexao_horario = '[' + str(desconexao.hour) + ':' + str(desconexao.minute) + ':' + str(desconexao.second) + ']'   
+    print(desconexao_horario, '[Conexão Finalizada]', cliente)
     conn.close()
