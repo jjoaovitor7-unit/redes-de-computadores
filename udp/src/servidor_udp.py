e0bde9bf841a1f057282bddcc7ef024d05745e84
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2019-05-27 15:07:03 -0300

    atualização em servidor_udp.py

--- udp/src/servidor_udp.py
+++ udp/src/servidor_udp.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import socket
+import datetime
 
-HOST = '127.0.0.1' # Endereco IP do Servidor
-PORT = 8080        # Porta que o Servidor esta
+HOST = '127.0.0.1' # Endereço IP do Servidor
+PORT = 8080        # Porta que o Servidor está
 
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 orig = (HOST, PORT)
@@ -11,6 +12,12 @@ udp.bind(orig)
 
 while True:
     msg, cliente = udp.recvfrom(1024)
+    cliente = str(cliente).replace('(', '[')
+    cliente = str(cliente).replace(')', ']')
+    
     if msg:
-        print(cliente, msg)
+        mensagem = datetime.datetime.now()
+        mensagem_horario = '[' + str(mensagem.hour) + ':' + str(mensagem.minute) + ':' + str(mensagem.second) + ']'
+        print(mensagem_horario, cliente, msg)
+
 udp.close()
